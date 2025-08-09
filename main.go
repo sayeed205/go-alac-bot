084a3afd0ac75db580ad246637db265218688c45
Hitarashi <sayeed205@gmail.com>
2025-08-09 14:02:55 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	API_ID := os.Getenv("API_ID")
+
+	fmt.Println("API_ID:", API_ID)
+
+}
